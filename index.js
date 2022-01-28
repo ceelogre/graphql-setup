9480f8e679fa566fa98b063d0b4ec8c9531a122f
ceelogre <sam.nishimwe@yahoo.com>
2022-01-28 11:34:23 +0200

    feat: graphql setup

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import pkg from 'graphql'
+const { buildSchema } = pkg
 const app = express()
+
+let schema = buildSchema(`
+type Query {
+  hello: String
+}
+`)
+
+let root = {
+  hello: () => 'I cry'
+}
 
 app.get('/', (req,res) => {
   res.json({
@@ -7,6 +20,12 @@ app.get('/', (req,res) => {
     author: 'Sarah X'
   })
 })
+
+app.use('/graph', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}))
 
 app.listen(3001, () => {
   console.info('3001')
